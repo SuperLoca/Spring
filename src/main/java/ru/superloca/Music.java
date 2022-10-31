7fd806a8d77fb0a0ad35311c7c686ccbdb04de99
KostyaBaushev <hearts_of_iron_4@mail.ru>
2022-10-31 14:56:07 +0300

    d

---
+++ src/main/java/ru/superloca/Music.java
@@ -0,0 +1,5 @@
+package ru.superloca;
+
+public interface Music {
+    String getSong();
+}
---
+++ src/main/java/ru/superloca/MusicPlayer.java
@@ -0,0 +1,14 @@
+package ru.superloca;
+
+public class MusicPlayer{
+    private Music music;
+
+    public MusicPlayer(Music music){
+        this.music = music;
+    }
+
+    public void PlayMusic(){
+        System.out.println("Playing: " + music.getSong() );
+    }
+
+}
